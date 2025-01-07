f913bc941fc3f3b2143d7d61ed6dc4052331dbf0
gilligan901 <charlie.michael.henderson@gmail.com>
2025-01-07 08:51:05 +0000

    removing weird foobar function

--- init.lua
+++ init.lua
@@ -368,13 +368,6 @@ require('lazy').setup({
       wk.add {
         { '<leader>f', group = 'file' }, -- group
         { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find File', mode = 'n' },
-        {
-          '<leader>fb',
-          function()
-            print 'hello'
-          end,
-          desc = 'Foobar',
-        },
         { '<leader>fn', desc = 'New File' },
         { '<leader>f1', hidden = true }, -- hide this keymap
         { '<leader>w', proxy = '<c-w>', group = 'windows' }, -- proxy to window mappings
